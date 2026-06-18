94c3a5ab636ec7672f3b3ba638439df30b8eedb8
Cleaner <1414984960@qq.com>
2026-06-18 09:56:24 +0800

    更新百度统计代码

--- src/.vuepress/client.ts
+++ src/.vuepress/client.ts
@@ -1,29 +1,31 @@
-import { defineClientConfig } from 'vuepress/client';
+import { defineClientConfig } from "vuepress/client";
 
 declare global {
-  interface Window { _hmt?: any[] }
+  interface Window {
+    _hmt?: any[];
+  }
 }
 
-const HM_SRC = 'https://hm.baidu.com/hm.js?7c4c1f680e6baeb4ec44730b3ad557f2';
+const HM_SRC = "https://hm.baidu.com/hm.js?ae30690bac70016c02255c734c21a136";
 
 function ensureHmScript() {
-  if (typeof document === 'undefined') return;
+  if (typeof document === "undefined") return;
   if (document.querySelector(`script[src="${HM_SRC}"]`)) return;
-  const hm = document.createElement('script');
+  const hm = document.createElement("script");
   hm.async = true;
   hm.src = HM_SRC;
-  const s = document.getElementsByTagName('script')[0];
+  const s = document.getElementsByTagName("script")[0];
   (s?.parentNode || document.head)?.insertBefore(hm, s || null);
 }
 
 export default defineClientConfig({
   enhance({ router }) {
-    if (typeof window === 'undefined') return;
+    if (typeof window === "undefined") return;
     window._hmt = window._hmt || [];
     ensureHmScript();
     router.afterEach((to) => {
       if (to?.fullPath) {
-        window._hmt?.push(['_trackPageview', to.fullPath]);
+        window._hmt?.push(["_trackPageview", to.fullPath]);
       }
     });
   },
